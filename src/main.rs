56da2aa82abb0698fef660561152ab7082538862
Ryan Chipman <ryan@ryanchipman.com>
2020-03-04 20:57:21 -0500

    basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{process, io::{self, Write}};
+
 fn main() {
-    println!("Hello, world!");
+	repl()
+}
+
+fn repl() {
+	loop {
+		print_prompt();
+		match read_input().as_str() {
+			".exit" => process::exit(0),
+			input => {
+				println!("unknown command '{}'", input);
+			},
+		};
+	}
+}
+
+fn read_input() -> String {
+	let mut input = String::new();
+	match io::stdin().read_line(&mut input) {
+		Ok(_) => {},
+		Err(e) => panic!(e),
+	};
+	input.trim().to_string()
+}
+
+fn print_prompt() {
+	print!("> ");
+	io::stdout().flush().expect("failed to flush stdout");
 }
