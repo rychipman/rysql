0d1c119978b8eb777141e96ddb6e4b9d578b94e0
Ryan Chipman <ryan@ryanchipman.com>
2020-06-14 11:46:14 -0400

    flesh out cst a bit more

--- src/parser.rs
+++ src/parser.rs
@@ -13,5 +13,18 @@ impl Expr {
 
 #[derive(Debug)]
 pub struct Select {
-
+	pub exprs: Vec<SelectExpr>,
+	pub from: Option<TableExpr>,
 }
+
+#[derive(Debug)]
+pub struct SelectExpr {
+	pub expr: Expr,
+	pub alias: Option<ColumnName>,
+}
+
+#[derive(Debug)]
+pub struct ColumnName(String);
+
+#[derive(Debug)]
+pub enum TableExpr {}
