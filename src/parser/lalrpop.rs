19eb981f62f1a70bab058ba3e7a164907c6c1df4
Ryan Chipman <ryan@ryanchipman.com>
2020-06-18 15:36:25 -0400

    expand grammar a bit

--- src/parser/lalrpop.rs
+++ src/parser/lalrpop.rs
@@ -1,4 +1,4 @@
-use crate::{ parser::cst, result::{Result, Error} };
+use crate::{parser::cst, result::Result};
 use lalrpop_util::{lexer::Token, lalrpop_mod};
 
 lalrpop_mod!(rysql);
@@ -6,7 +6,7 @@ lalrpop_mod!(rysql);
 pub type ParseError<'t> = lalrpop_util::ParseError<usize, Token<'t>, &'static str>;
 
 pub fn parse(input: &str) -> Result<cst::Expr> {
-	let select = rysql::SelectParser::new().parse(input).unwrap();
+	let select = rysql::SelectParser::new().parse(input)?;
 	let expr = cst::Expr::Select(select);
 	Ok(expr)
 }
